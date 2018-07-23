0d4ef53baac55fdf10c4695cb3fcc441a87789fe
Kurt Obispo <kurtobispo@gmail.com>
2018-07-23 23:57:39 +0800

    Added playful intro.

---
+++ src/page-styles/index.style.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+// import { Link } from 'gatsby'
+import media from '../media'
+
+export const Wrapper = styled.div`
+  padding: 50px;
+
+  ${media.lessThan('md')`
+    padding: 30px;
+  `};
+
+  ${media.lessThan('sm')`
+    padding: 15px;
+  `};
+`
+
+export const IntroParagraph = styled.p`
+  color: ${props => props.theme.lightColor};
+  font-family: ${props => props.theme.bodyFont};
+  font-weight: 500;
+  font-size: 48px;
+  line-height: 1.25em;
+
+  ${media.lessThan('md')`
+    font-size: 38px;
+  `};
+
+  ${media.lessThan('sm')`
+    font-size: 32px;
+  `};
+`
+
+export const Emphasize = styled.em`
+  color: ${props => props.theme.primaryColor};
+  font-style: normal;
+`
+
+export const Strikethrough = styled.span`
+  text-decoration: line-through;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import Layout from '../components/Layouts/general-layout'
 import Coming from '../components/Coming'
+import {
+  Wrapper,
+  IntroParagraph,
+  Emphasize,
+  Strikethrough
+} from '../page-styles/index.style'
 require('typeface-butler')
 require('typeface-montserrat')
 
 const IndexPage = () => (
   <Layout>
-    <Coming />
+    <Wrapper>
+      <IntroParagraph>Hello There!</IntroParagraph>
+      <IntroParagraph>
+        I yam <Emphasize>Shelby Sage Lisieski</Emphasize>. I’m a dingus, I love
+        to dance, and I work as an assface. I’m currently living in{' '}
+        <Strikethrough>Avon</Strikethrough>{' '}
+        <Strikethrough>Philippines</Strikethrough>{' '}
+        <Strikethrough>Rochester</Strikethrough> <Emphasize>Boston</Emphasize>.
+        Ben Howard, walking, peanut butter—these are few of my favorite things.
+      </IntroParagraph>
+    </Wrapper>
   </Layout>
 )
 
